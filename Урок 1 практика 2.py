208de29114ed84c59a135d0b6d0c1a38132b9da3
dubsteptc@mail.ru <dubsteptc@mail.ru>
2023-01-12 07:11:10 +0300

    4

---
+++ Урок 1 практика 2.py
@@ -0,0 +1,14 @@
+var_int = 10
+print(var_int)
+var_float = 8.4
+print(var_float)
+var_str = 'No'
+print(var_str)
+var_big = var_int * 3.5
+print(var_big)
+var_float = var_float - 1
+print(var_float)
+var_str = 'NoNoYesYesYes'
+print(var_str)
+print(var_int / var_float)
+print(var_big / var_float)
